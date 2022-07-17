53cc19e2ab66f4bfcc05f0c015ce435900ce8a0b
Matheus Chonge <matheus.chong@gmail.com>
2022-07-17 20:03:00 -0300

    feat: main router and database client

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,12 @@ import 'express-async-errors'
 import express,{json} from 'express'
 import cors from 'cors';
 
+import router from '../routes/mainRouter.js'
+
 const app = express();
 
 app.use(cors())
 app.use(json())
+app.use(router)
 
 export default app;
